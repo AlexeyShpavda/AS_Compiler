79d81e52be7300e0d8f47d064fe996c017d29968
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-03-20 23:08:04 +0300

    Create AS_Compiler solution

---
+++ AS_Compiler.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AS_Compiler.CommandLine
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
