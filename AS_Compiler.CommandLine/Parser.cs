360547254a89cd4f2d41a800e13cf9f2ce4be298
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-03-24 19:42:33 +0300

    Add Parser

---
+++ AS_Compiler.CommandLine/Parser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace AS_Compiler.CommandLine
+{
+    public class Parser
+    {
+        private readonly SyntaxToken[] _syntaxTokens;
+        private int _position;
+
+        public Parser(string text)
+        {
+            var syntaxTokens = new List<SyntaxToken>();
+
+            var lexer = new Lexer(text);
+            SyntaxToken syntaxToken;
+
+            do
+            {
+                syntaxToken = lexer.NextSyntaxToken();
+
+                if (syntaxToken.Type != SyntaxType.WhiteSpace && syntaxToken.Type != SyntaxType.Unknown)
+                {
+                    syntaxTokens.Add(syntaxToken);
+                }
+            } while (syntaxToken.Type != SyntaxType.EndOfFile);
+
+            _syntaxTokens = syntaxTokens.ToArray();
+        }
+
+        private SyntaxToken Peek(int offset)
+        {
+            var index = _position + offset;
+
+            return index >= _syntaxTokens.Length ? _syntaxTokens[^1] : _syntaxTokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+    }
+}
