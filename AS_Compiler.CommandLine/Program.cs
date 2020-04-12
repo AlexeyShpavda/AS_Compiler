cd0a2c786a9316c28ae133e2a1507762f0eb2478
ALEXEY SHPAVDA <alexeyshpavda@gmail.com>
2020-04-12 18:43:27 +0300

    Upgrade REPL

--- AS_Compiler.CommandLine/Program.cs
+++ AS_Compiler.CommandLine/Program.cs
@@ -1,126 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AS_Compiler.Core.CodeAnalysis;
-using AS_Compiler.Core.CodeAnalysis.Syntax;
-using AS_Compiler.Core.CodeAnalysis.Text;
-
-namespace AS_Compiler.CommandLine
+﻿namespace AS_Compiler.CommandLine
 {
     internal class Program
     {
         private static void Main()
         {
-            var showTree = false;
-            var showProgram = false;
-            var variables = new Dictionary<VariableSymbol, object>();
-            var textBuilder = new StringBuilder();
-            Compilation previous = null;
+            var readEvalPrintLoop = new CompilerReadEvalPrintLoop();
 
-            while (true)
-            {
-                Console.Write(textBuilder.Length == 0 ? "> " : "| ");
-
-                var input = Console.ReadLine();
-                var isLineBlank = string.IsNullOrWhiteSpace(input);
-
-                if (textBuilder.Length == 0)
-                {
-                    if (isLineBlank)
-                        break;
-
-                    switch (input)
-                    {
-                        case "#showTree":
-                            showTree = !showTree;
-                            Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees.");
-                            continue;
-                        case "#showProgram":
-                            showProgram = !showProgram;
-                            Console.WriteLine(showProgram ? "Showing parse trees." : "Not showing bound tree.");
-                            continue;
-                        case "#cls":
-                            Console.Clear();
-                            continue;
-                        case "#reset":
-                            previous = null;
-                            continue;
-                    }
-                }
-
-                textBuilder.AppendLine(input);
-
-                var text = textBuilder.ToString();
-                var syntaxTree = SyntaxTree.Parse(text);
-
-                if (!isLineBlank && syntaxTree.Diagnostics.Any())
-                    continue;
-
-                var compilation = previous == null
-                    ? new Compilation(syntaxTree)
-                    : previous.ContinueWith(syntaxTree);
-
-                if (showTree)
-                {
-                    syntaxTree.Root.WriteTo(Console.Out);
-                }
-
-                if (showProgram)
-                {
-                    compilation.EmitTree(Console.Out);
-                }
-
-                var result = compilation.Evaluate(variables);
-
-                var diagnostics = result.Diagnostics;
-
-                if (!diagnostics.Any())
-                {
-                    Console.WriteLine(result.Value);
-
-                    previous = compilation;
-                }
-                else
-                {
-                    foreach (var diagnostic in diagnostics)
-                    {
-                        var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.TextSpan.Start);
-                        var line = syntaxTree.Text.Lines[lineIndex];
-                        var lineNumber = lineIndex + 1;
-                        var character = diagnostic.TextSpan.Start - line.Start + 1;
-
-                        Console.WriteLine();
-
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.Write($"({lineNumber}, {character}): ");
-                        Console.WriteLine(diagnostic);
-                        Console.ResetColor();
-
-                        var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.TextSpan.Start);
-                        var suffixSpan = TextSpan.FromBounds(diagnostic.TextSpan.End, line.End);
-
-                        var prefix = syntaxTree.Text.ToString(prefixSpan);
-                        var error = syntaxTree.Text.ToString(diagnostic.TextSpan);
-                        var suffix = syntaxTree.Text.ToString(suffixSpan);
-
-                        Console.Write("    ");
-                        Console.Write(prefix);
-
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.Write(error);
-                        Console.ResetColor();
-
-                        Console.Write(suffix);
-
-                        Console.WriteLine();
-                    }
-
-                    Console.WriteLine();
-                }
-
-                textBuilder.Clear();
-            }
+            readEvalPrintLoop.Run();
         }
     }
 }
